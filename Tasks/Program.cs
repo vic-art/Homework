6e6fcca82c8d4f70752017d40ee5271938306549
Victoria Artyushkina <victoriaartyushkina@gmail.com>
2022-10-06 01:59:44 +0200

    add the solution to the 2nd task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное
+// из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a, b, c;
+
+Console.WriteLine("Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.WriteLine("Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.WriteLine("Введите третье число: ");
+int.TryParse(Console.ReadLine()!, out c);
+
+if (a > b)
+    if (a > c) 
+    {
+        Console.WriteLine($"{a}");
+    }
+    else
+    {
+        Console.WriteLine($"{c}");
+    }
+else if (b > c)
+{
+    Console.WriteLine($"{b}");
+}
+else
+{
+    Console.WriteLine($"{c}");
+}
+
